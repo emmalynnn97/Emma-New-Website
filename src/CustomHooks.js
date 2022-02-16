4a53dd8963950720cf1b14df1339d4584a2ff56b
Emma Guy <emmalynnn97@gmail.com>
2022-02-16 01:47:43 -0700

    make email form work

--- src/CustomHooks.js
+++ src/CustomHooks.js
@@ -1,26 +1,26 @@
 import { useState } from 'react'
 import axios from 'axios'
 const useContactForm = () => {
-    const [inputs, setInputs] = useState({
-      firstName: '',
-      email:'',
-      subject: '',
-      message: ''
-    });
-    const handleInputChange = (event) => {
-      setInputs(inputs => ({...inputs, [event.target.name]: event.target.value}));
-    }
-    const handleSubmit = (event) => {
-      event.preventDefault()
-      axios.post('http://localhost:5001/contact', inputs)
-        .then(response=>console.log(response))
-        .catch(error=>console.log(error))
-    }
-    return {
-      handleSubmit,
-      handleInputChange,
-      inputs
-    };
+  const [inputs, setInputs] = useState({
+    firstName: '',
+    email: '',
+    subject: '',
+    message: ''
+  });
+  const handleInputChange = (event) => {
+    setInputs(inputs => ({ ...inputs, [event.target.name]: event.target.value }));
   }
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    axios.post('https://emma-form-server.herokuapp.com/contact', inputs)
+      .then(response => console.log(response))
+      .catch(error => console.log(error))
+  }
+  return {
+    handleSubmit,
+    handleInputChange,
+    inputs
+  };
+}
 
 export default useContactForm
